476e6ef7846d0375f540e71339a181a6b4e00b9b
Fletch17 <89846786+Fletch17@users.noreply.github.com>
2022-05-08 21:58:54 +0600

    init

--- 02 StatementsAndLoops/03_Sequence/03_Sequence/Program.cs
+++ 02 StatementsAndLoops/03_Sequence/03_Sequence/Program.cs
@@ -7,14 +7,11 @@ namespace _03_Sequence
         static void Main(string[] args)
         {
             int startNumber = 5;
-            int step = 7;
             int stopNumber = 96;
-            int number = startNumber;
 
-            while (number <= stopNumber)
+            for (int i = startNumber; i <= stopNumber; i += 7)
             {
-                Console.WriteLine(number);
-                number += step;
+                Console.WriteLine(i);
             }
         }
     }
