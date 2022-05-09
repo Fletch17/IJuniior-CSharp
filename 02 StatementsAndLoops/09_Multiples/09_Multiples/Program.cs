f14f0fe415e77a4c4230b421150aba41d18de3c6
Fletch17 <89846786+Fletch17@users.noreply.github.com>
2022-05-09 15:48:57 +0600

    init

---
+++ 02 StatementsAndLoops/09_Multiples/09_Multiples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _09_Multiples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int startNumber = 1;
+            int endNumber = 27;
+            int number = random.Next(startNumber, endNumber + 1);
+            int multilesCount = 0;
+
+            Console.WriteLine($"Ваше число {number}");
+
+            for (int i = number; i < 1000; i += number)
+            {
+                if (i >= 100)
+                {
+                    multilesCount++;
+                }
+            }
+
+            Console.WriteLine($"Кол-во кратных чисел - {multilesCount}.");
+        }
+    }
+}
