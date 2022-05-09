d0f6166426d9876e626683444bf6c0f04595bb17
Fletch17 <89846786+Fletch17@users.noreply.github.com>
2022-05-09 15:12:19 +0600

    init

---
+++ 02 StatementsAndLoops/07_NameInSquare/07_NameInSquare/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _07_NameInSquare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string symbol;
+            int letterCount;
+
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите символ: ");
+            symbol = Console.ReadLine();
+
+            letterCount = name.Length;
+
+            for (int i = 0; i < letterCount + 2; i++)
+            {
+                Console.Write(symbol);
+            }
+
+            Console.Write("\n" + symbol);
+            Console.Write(name);
+            Console.Write(symbol + "\n");
+
+            for (int i = 0; i < letterCount + 2; i++)
+            {
+                Console.Write(symbol);
+            }
+        }
+    }
+}
