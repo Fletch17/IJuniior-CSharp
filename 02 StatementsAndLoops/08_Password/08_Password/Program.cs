0be94257e54e5729dcb2b2b8f6d58497cc4ee142
Fletch17 <89846786+Fletch17@users.noreply.github.com>
2022-05-09 15:33:31 +0600

    init

---
+++ 02 StatementsAndLoops/08_Password/08_Password/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08_Password
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "olo";
+            int numberOfAttempts = 3;
+            string message = "Gratz!";
+            string userInput;
+
+            for (int i = 0; i < numberOfAttempts; i++)
+            {
+                Console.Write("Введите пароль: ");
+                userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    Console.WriteLine(message);
+                    return;
+                }
+
+                Console.WriteLine("Неправильный пароль");
+            }
+        }
+    }
+}
