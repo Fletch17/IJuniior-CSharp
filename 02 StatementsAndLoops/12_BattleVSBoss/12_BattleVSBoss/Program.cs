d788c5c3587638fa62983be82c93254f1c6750f3
Fletch17 <89846786+Fletch17@users.noreply.github.com>
2023-04-07 20:11:39 +0600

    init

--- 02 StatementsAndLoops/12_BattleVSBoss/12_BattleVSBoss/Program.cs
+++ 02 StatementsAndLoops/12_BattleVSBoss/12_BattleVSBoss/Program.cs
@@ -4,6 +4,10 @@
     {
         static void Main(string[] args)
         {
+            const int fireBall = 1;
+            const int fireFenix = 2;
+            const int fireWall = 3;
+            const int fireRain = 4;
             Random random = new Random();
             string userInput;
             int userHealth = 500;
@@ -24,58 +28,64 @@
             string fireRainName = "Огненный ливень";
             int fireRainDamage = 100;
             bool isFireRainCombo = false;
+            int fireRainMultiplier = 3;
+            int fireRainMultiplierChance = 50;
 
             string fireBallName = "Шар огня";
             int fireBallDamage = 75;
             bool isFireBallCombo = false;
+            int fireBallMultiplier = 2;
 
             Console.WriteLine("Вы маг огня и перед Вами босс. Ваши действия?");
 
             while (isBattle)
             {
                 Console.WriteLine("Ваше здоровье: {0}\nЗдоровье босса: {1}\n", userHealth, bossHealth);
-                Console.WriteLine("1. {0} (серия наносит двойной урон, при использовании другого навыка серия прерывается) - {1} урона.", fireBallName, fireBallDamage);
-                Console.WriteLine("2. {0} - {1} урона.", fireFenixName, fireFenixDamage);
-                Console.WriteLine("3. {0} (дает 50% шанс нанести тройной урон навыком Огненный ливень) - {1} урона.", fireWallName, fireWallDamage);
-                Console.WriteLine("4. {0} - {1} урона.", fireRainName, fireRainDamage);
-                isChoiseCorrect = false;
+                Console.WriteLine("{0}. {1} (серия наносит урон x{2}, при использовании другого навыка серия прерывается) - {3} урона.", fireBall, fireBallName, fireBallMultiplier, fireBallDamage);
+                Console.WriteLine("{0}. {1} - {2} урона.", fireFenix, fireFenixName, fireFenixDamage);
+                Console.WriteLine("{0}. {1} (дает {2}% шанс нанести урон x{3} навыком Огненный ливень) - {4} урона.", fireWall, fireWallName, fireRainMultiplierChance, fireRainMultiplier, fireWallDamage);
+                Console.WriteLine("{0}. {1} - {2} урона.", fireRain, fireRainName, fireRainDamage);
+                isChoiseCorrect = true;
 
-                while (!isChoiseCorrect)
+                while (isChoiseCorrect)
                 {
                     Console.Write("Выберите умение: ");
                     userInput = Console.ReadLine();
-                    isChoiseCorrect = true;
+                    isChoiseCorrect = false;
 
                     switch (userInput)
                     {
                         case "1":
-                            if (!isFireBallCombo)
+                            if (isFireBallCombo)
                             {
-                                userDamage = fireBallDamage;
+                                userDamage = fireBallDamage * fireBallMultiplier;
                             }
                             else
                             {
-                                userDamage = fireBallDamage * 2;
+                                userDamage = fireBallDamage;
                             }
 
                             isFireBallCombo = true;
                             isFireRainCombo = false;
                             break;
+
                         case "2":
                             isFireBallCombo = false;
                             userDamage = fireFenixDamage;
                             break;
+
                         case "3":
                             isFireBallCombo = false;
                             isFireRainCombo = true;
                             userDamage = fireWallDamage;
                             break;
+
                         case "4":
                             isFireBallCombo = false;
 
-                            if (isFireRainCombo && random.Next(100) > 50)
+                            if (isFireRainCombo && random.Next(100) > fireRainMultiplierChance)
                             {
-                                userDamage = fireRainDamage * 3;
+                                userDamage = fireRainDamage * fireRainMultiplier;
                             }
                             else
                             {
@@ -84,9 +94,10 @@
 
                             isFireRainCombo = false;
                             break;
+
                         default:
                             Console.WriteLine("Введите номер пункта из предложенных.");
-                            isChoiseCorrect = false;
+                            isChoiseCorrect = true;
                             break;
                     }
                 }
@@ -98,24 +109,26 @@
                 Console.WriteLine("Вы используете: {0} и наносите {1} урона", fireBallName, userDamage);
                 Console.WriteLine("Босс наносит вам: {0} урона\n", bossDamage);
 
-                if (userHealth <= 0 && bossHealth > 0)
+                if (userHealth <= 0 || bossHealth <= 0)
                 {
-                    Console.WriteLine("Вы проиграли.");
-                    isBattle = false;
-                }
-                else if (userHealth > 0 && bossHealth <= 0)
-                {
-                    Console.WriteLine("Вы победили.");
-                    isBattle = false;
-                }
-                else if (userHealth <= 0 && bossHealth <= 0)
-                {
-                    Console.WriteLine("Вы оба погибли.");
                     isBattle = false;
                 }
             }
 
+            if (userHealth <= 0 && bossHealth > 0)
+            {
+                Console.WriteLine("Вы проиграли.");
+            }
+            else if (userHealth > 0 && bossHealth <= 0)
+            {
+                Console.WriteLine("Вы победили.");
+            }
+            else if (userHealth <= 0 && bossHealth <= 0)
+            {
+                Console.WriteLine("Вы оба погибли.");
+            }
+
             Console.WriteLine("Ваше здоровье: {0}\nЗдоровье босса: {1}\n", userHealth, bossHealth);
         }
-    }    
+    }
 }
