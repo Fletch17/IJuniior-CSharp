867c31db710f7c4aed0316ef14fbb3130b280b61
Fletch17 <89846786+Fletch17@users.noreply.github.com>
2023-04-08 00:06:34 +0600

    init

---
+++ 03 Array/01_WorkWithRows/01_WorkWithRows/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _01_WorkWithRows
+{
+    //Дан двумерный массив. 322
+    //Вычислить сумму второй строки и произведение первого столбца.Вывести исходную матрицу и результаты вычислений.
+
+    internal class Program
+    {
+        static Random random;
+
+        static void Main(string[] args)
+        {
+            random = new Random();
+            int[,] array = new int[5, 5];
+            int summ = 0;
+            int productOfNumbers = 1;
+            int row = 1;
+            int column = 0;            
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(0, 30);
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (i == 1)
+                    {
+                        summ += array[i, j];
+                    }
+
+                    if (j == 0)
+                    {
+                        productOfNumbers *= array[i, j];
+                    }
+                }
+            }
+
+            Console.WriteLine("Сумма строки #{0} - {1}", row, summ);
+            Console.WriteLine("Произведение столбца #{0} - {1}", column, productOfNumbers);
+        }
+    }
+}
