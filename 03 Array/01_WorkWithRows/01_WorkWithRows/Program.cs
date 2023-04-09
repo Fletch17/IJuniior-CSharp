82510402c75e4eddf73df0005d694af61f8a155b
Fletch17 <89846786+Fletch17@users.noreply.github.com>
2023-04-09 22:10:47 +0600

    init

--- 03 Array/01_WorkWithRows/01_WorkWithRows/Program.cs
+++ 03 Array/01_WorkWithRows/01_WorkWithRows/Program.cs
@@ -4,20 +4,30 @@
     {
         static void Main(string[] args)
         {
+            const int rowCount = 5;
+            const int columnCount = 5;
+
             Random random = new Random();
-            int minimumValue = 0;
-            int maximumValue = 6;
-            int[,] numbers = new int[5, 5];
+            int minimumValueInNumbersArray = 0;
+            int maximumValueInNumbersArray = 6;
+            int[,] numbers = new int[rowCount, columnCount];
             int summ = 0;
             int productOfNumbers = 1;
-            int row = 1;
-            int column = 0;
+            int roworSearch = 1;
+            int columnForSearch = 0;
 
             for (int i = 0; i < numbers.GetLength(0); i++)
             {
                 for (int j = 0; j < numbers.GetLength(1); j++)
                 {
-                    numbers[i, j] = random.Next(minimumValue, maximumValue);
+                    numbers[i, j] = random.Next(minimumValueInNumbersArray, maximumValueInNumbersArray);
+                }
+            }
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
                     Console.Write(numbers[i, j] + "\t");
                 }
 
@@ -26,16 +36,16 @@
 
             for (int i = 0; i < numbers.GetLength(1); i++)
             {
-                summ += numbers[row, i];
+                summ += numbers[roworSearch, i];
             }
 
             for (int i = 0; i < numbers.GetLength(0); i++)
             {
-                productOfNumbers *= numbers[i, column];
+                productOfNumbers *= numbers[i, columnForSearch];
             }
 
-            Console.WriteLine("Сумма строки #{0} - {1}", row, summ);
-            Console.WriteLine("Произведение столбца #{0} - {1}", column, productOfNumbers);
+            Console.WriteLine("Сумма строки #{0} - {1}", roworSearch, summ);
+            Console.WriteLine("Произведение столбца #{0} - {1}", columnForSearch, productOfNumbers);
         }
     }
 }
