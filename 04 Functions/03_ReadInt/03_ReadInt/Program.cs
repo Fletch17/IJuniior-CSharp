887eafc2ba73636fb7743d593bb911417a2537bf
Fletch17 <89846786+Fletch17@users.noreply.github.com>
2023-04-27 21:56:19 +0600

    init

---
+++ 04 Functions/03_ReadInt/03_ReadInt/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _03_ReadInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            number = ReadInt();
+            Console.WriteLine("Ваше число: " + number);
+        }
+
+        static int ReadInt()
+        {
+            string userInput;
+            bool isInt = false;
+            int number = 0;
+
+            while (!isInt)
+            {
+                Console.Write("Введите число: ");
+                userInput = Console.ReadLine();
+                isInt = int.TryParse(userInput, out number);
+            }
+
+            return number;
+        }
+    }
+}
