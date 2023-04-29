1e877a707991ab1ef63dc46322666ac1139ac7b1
Fletch17 <89846786+Fletch17@users.noreply.github.com>
2023-04-29 21:37:48 +0600

    init

---
+++ 05 Collections/01_Dictionary/01_Dictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01_Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add("ball", "мяч");
+            dictionary.Add("sword", "меч");
+            dictionary.Add("red", "красный");
+            dictionary.Add("black", "черный");
+            dictionary.Add("sun", "солнце");
+
+            userInput = Console.ReadLine();
+
+            if (dictionary.ContainsKey(userInput))
+            {
+                Console.WriteLine(dictionary[userInput]);
+            }
+            else
+            {
+                Console.WriteLine("Такого слова нет.");
+            }
+
+
+        }
+    }
+}
