553be41817b434e1fe9ad872310f4ec7588a5af6
Fletch17 <89846786+Fletch17@users.noreply.github.com>
2023-05-01 22:04:33 +0600

    init

---
+++ 05 Collections/02_Queue/02_Queue/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _02_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            int money = 0;
+            int numberCount = 10;
+
+            for (int i = 0; i < numberCount; i++)
+            {
+                queue.Enqueue(i);
+            }
+
+            while (queue.Count != 0)
+            {
+                money += queue.Dequeue();
+                Console.WriteLine($"Ваш баланс {money}");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
