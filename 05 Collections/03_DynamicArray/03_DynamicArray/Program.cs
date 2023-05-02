692f4e92317b934597579346b3dcf86f8a89c4ba
Fletch17 <89846786+Fletch17@users.noreply.github.com>
2023-05-02 20:02:13 +0600

    init

---
+++ 05 Collections/03_DynamicArray/03_DynamicArray/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _03_DynamicArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            bool isProgramWork = true;
+            string commandExit = "exit";
+            string commandSum = "sum";
+            string userInput;
+            int sum;
+
+            while (isProgramWork)
+            {
+                userInput = Console.ReadLine();
+                bool isInt = int.TryParse(userInput, out int index);
+
+                if (isInt)
+                {
+                    numbers.Add(index);
+                }
+                else 
+                {
+                    if (userInput == commandSum)
+                    {
+                        sum = 0;
+
+                        foreach (int number in numbers)
+                        {
+                            sum += number;
+                        }
+
+                        Console.WriteLine($"Сумма равна: {sum}");
+                    }
+                    else if (userInput == commandExit)
+                    {
+                        isProgramWork = false;
+                    }
+                }
+            }
+        }
+    }
+}
