42625cd1396b1b6f8e02617a16ba326f4ef44622
Fletch17 <89846786+Fletch17@users.noreply.github.com>
2023-05-04 20:06:37 +0600

    init

---
+++ 06 OOP/01_WorkingWithClasses/01_WorkingWithClasses/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _01_WorkingWithClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Alex", 100, 23);
+
+            player.ShowInformation();
+        }
+    }
+
+    class Player
+    {
+        private string _name;
+        private int _health;
+        private int _gold;
+
+        public Player(string name, int healt, int gold)
+        {
+            _name = name;
+            _health = healt;
+            _gold = gold;
+        }
+
+        public void ShowInformation()
+        {
+            Console.WriteLine($"{_name}. Здоровье - {_health}, золото - {_gold}");
+        }
+    }
+}
