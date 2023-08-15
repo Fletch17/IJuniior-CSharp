253c108b2d9c994f3d0165ffc0d82bc2eb2068c2
Fletch17 <sylar-gray@mail.ru>
2023-08-15 20:16:03 +0600

    init

--- 06 OOP/07_Trains/07_Trains/Program.cs
+++ 06 OOP/07_Trains/07_Trains/Program.cs
@@ -1,13 +1,5 @@
 ﻿namespace _07_Trains
 {
-    //У вас есть программа, коротая помогает пользователю составить план поезда.
-    //    Есть 4 основных шага в создании: 
-    //    -Создать направление - создает направление для поезда (к примеру Бийск-Барнаул)
-    //    -Продать билеты - вы получаете рандомное кол-во пассажиров, которые купили билеты на это направление.
-    //    -Сформировать поезд - вы создаете поезд и добавляете ему столько вагонов(вагоны могут быть разные по вместимости),
-    //    сколько хватит для перевозки всех пассажиров.
-    //    -Отправить поезд - вы отправляете поезд, после чего можете снова создать направление.
-    //В верхней части программы должна выводиться полная информация о текущем рейсе или его отсутствии.
     internal class Program
     {
         static void Main(string[] args)
@@ -18,12 +10,12 @@
 
     public class Station
     {
-        private List <string> _destinations;
+        private List<string> _destinations;
         private Train _train;
 
         public Station()
         {
-            List<string> cities= new List<string> { "Москва", "Сочи", "Хабаровск", "Новосибирск" };
+            List<string> cities = new List<string> { "Москва", "Сочи", "Хабаровск", "Новосибирск" };
             CreateDestinations(cities);
         }
 
@@ -34,12 +26,12 @@
             int min = 150;
             int max = 250;
             int index;
-            Random random= new Random();
+            Random random = new Random();
 
-            while(currentTrain!= trainCountMax)
+            while (currentTrain != trainCountMax)
             {
-                index=random.Next(_destinations.Count);
-                _train = new Train(min,max);
+                index = random.Next(_destinations.Count);
+                _train = new Train(min, max);
 
                 Console.WriteLine($"\tНаправление: {_destinations[index]}.");
                 Console.WriteLine($"Кол-во пассажиров: {_train.PasengersCount}.");
@@ -48,7 +40,7 @@
 
                 for (int i = 0; i < _train.CarriagesCount; i++)
                 {
-                    Console.WriteLine($"{i+1} вагон: {_train.GetCarriage(i).Capacity}.");
+                    Console.WriteLine($"{i + 1} вагон: {_train.GetCarriage(i).Capacity}.");
                 }
 
                 currentTrain++;
@@ -78,43 +70,43 @@
     {
         private List<Carriage> _carriages;
 
-        public Train(int min,int max)
+        public Train(int min, int max)
         {
-            Random random= new Random();
-            PasengersCount=random.Next(min,max);
+            Random random = new Random();
+            PasengersCount = random.Next(min, max);
             _carriages = new List<Carriage>();
-            FillVagon();
+            FillCarriage();
         }
-        
+
         public int PasengersCount { get; private set; }
-        public int CarriagesCount=>_carriages.Count;
+        public int CarriagesCount => _carriages.Count;
 
         public Carriage GetCarriage(int index)
         {
             return _carriages[index];
         }
-        
-        private void FillVagon()
+
+        private void FillCarriage()
         {
             int min = 15;
             int max = 30;
             int people = PasengersCount;
 
-            while(people>0)
+            while (people > 0)
             {
-                Carriage vagon=new Carriage(min,max);
-                _carriages.Add(vagon);
-                people -= vagon.Capacity;
+                Carriage carriage = new Carriage(min, max);
+                _carriages.Add(carriage);
+                people -= carriage.Capacity;
             }
         }
     }
 
     public class Carriage
     {
-        public Carriage(int min,int max)
+        public Carriage(int min, int max)
         {
-            Random random= new Random();
-            Capacity=random.Next(min,max);
+            Random random = new Random();
+            Capacity = random.Next(min, max);
         }
 
         public int Capacity { get; private set; }
