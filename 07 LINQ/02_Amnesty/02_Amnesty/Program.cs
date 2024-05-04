b204f1b73fcd6c35c7ef152d970d7fc82124a76f
Fletch17 <89846786+Fletch17@users.noreply.github.com>
2024-05-04 21:37:21 +0600

    init

---
+++ 07 LINQ/02_Amnesty/02_Amnesty/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace _02_Amnesty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Criminal> criminals = new List<Criminal>()
+            {new Criminal("Гоша", "Воровство"),
+            new Criminal("Тоша", "Антиправительственное"),
+            new Criminal("Артем", "Воровство"),
+            new Criminal("Андрей", "Антиправительственное"),
+            new Criminal("Дмитрий", "Убийство") };
+
+            var filteredCriminals = from Criminal criminal in criminals 
+                                    where criminal.Crime!= "Антиправительственное"
+                                    select criminal;
+
+            ShowInfo("До амнистии:", criminals);
+            Console.WriteLine();
+            ShowInfo("После амнистии:", filteredCriminals);
+        }
+
+        static void ShowInfo(string message, IEnumerable<Criminal> criminals)
+        {
+            Console.WriteLine(message);
+
+            foreach (var criminal in criminals)
+            {
+                Console.WriteLine(criminal.Name);
+            }
+        }
+    }
+
+    public class Criminal
+    {
+        public string Name { get; private set; }
+        public string Crime { get; private set; }
+
+        public Criminal(string name, string crime)
+        {
+            Name = name;
+            Crime = crime;
+        }
+    }
+}
